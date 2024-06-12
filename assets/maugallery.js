9659826a95b8c97f5df50daf1d9db0f0dfcbfed6
Piama <athelec@gmail.com>
2024-06-12 17:27:47 +0200

    Correction des bugs filtres surbrillance - click photos modale

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -119,82 +119,33 @@
         .attr("src", element.attr("src"));
       $(`#${lightboxId}`).modal("toggle");
     },
+// FONCTION DE L'IMAGE PRECEDENTE
     prevImage() {
-      let activeImage = null;
-      $("img.gallery-item").each(function() {
-        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
-        }
-      });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
+      let activeImage = $(".lightboxImage").attr("src");
       let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function() {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
-      let index = 0,
-        next = null;
-
-      $(imagesCollection).each(function(i) {
-        if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i ;
-        }
+      $(".gallery-item:visible").each(function() {
+        imagesCollection.push($(this).attr("src"));
       });
-      next =
-        imagesCollection[index] ||
-        imagesCollection[imagesCollection.length - 1];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+    
+      let currentIndex = imagesCollection.indexOf(activeImage);
+      let prevIndex = (currentIndex - 1 + imagesCollection.length) % imagesCollection.length;
+    
+      $(".lightboxImage").attr("src", imagesCollection[prevIndex]);
     },
+// FONCTION DE L'IMAGE SUIVANTE
     nextImage() {
-      let activeImage = null;
-      $("img.gallery-item").each(function() {
-        if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
-          activeImage = $(this);
-        }
-      });
-      let activeTag = $(".tags-bar span.active-tag").data("images-toggle");
+      let activeImage = $(".lightboxImage").attr("src");
       let imagesCollection = [];
-      if (activeTag === "all") {
-        $(".item-column").each(function() {
-          if ($(this).children("img").length) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      } else {
-        $(".item-column").each(function() {
-          if (
-            $(this)
-              .children("img")
-              .data("gallery-tag") === activeTag
-          ) {
-            imagesCollection.push($(this).children("img"));
-          }
-        });
-      }
-      let index = 0,
-        next = null;
-
-      $(imagesCollection).each(function(i) {
-        if ($(activeImage).attr("src") === $(this).attr("src")) {
-          index = i;
-        }
+      $(".gallery-item:visible").each(function() {
+        imagesCollection.push($(this).attr("src"));
       });
-      next = imagesCollection[index] || imagesCollection[0];
-      $(".lightboxImage").attr("src", $(next).attr("src"));
+    
+      let currentIndex = imagesCollection.indexOf(activeImage);
+      let nextIndex = (currentIndex + 1) % imagesCollection.length;
+    
+      $(".lightboxImage").attr("src", imagesCollection[nextIndex]);
     },
+
     createLightBox(gallery, lightboxId, navigation) {
       gallery.append(`<div class="modal fade" id="${
         lightboxId ? lightboxId : "galleryLightbox"
@@ -239,25 +190,24 @@
       if ($(this).hasClass("active-tag")) {
         return;
       }
-      $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active-tag");
-
+      // Retirer la classe active des autres boutons
+      $(".tags-bar .nav-link").removeClass("active active-tag");
+      // Ajouter la classe active au bouton cliqué
+      $(this).addClass("active active-tag");
+    
       var tag = $(this).data("images-toggle");
-
+    
+      // Filtrer les éléments de la galerie en fonction du tag
       $(".gallery-item").each(function() {
-        $(this)
-          .parents(".item-column")
-          .hide();
+        $(this).parents(".item-column").hide();
         if (tag === "all") {
-          $(this)
-            .parents(".item-column")
-            .show(300);
+          $(this).parents(".item-column").show(300);
         } else if ($(this).data("gallery-tag") === tag) {
-          $(this)
-            .parents(".item-column")
-            .show(300);
+          $(this).parents(".item-column").show(300);
         }
       });
     }
   };
 })(jQuery);
+
+
